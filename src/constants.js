15389ecad9c4147343fe73073b89b5b8455117ad
Lucy Hao <hao.lucyy@gmail.com>
2024-01-03 00:12:18 -0700

    done team

--- src/constants.js
+++ src/constants.js
@@ -12,3 +12,250 @@ export const links = [
 export const Padding = styled.div`
   padding: 5px 15vw;
 `;
+
+export const Members = [
+  {
+    header: "Directors",
+    people: [
+      {
+        name: "Chaehyeon Lee",
+        photo: "",
+        position: "Co-Director",
+        major: "Biomedical Engineering",
+      },
+      {
+        name: "Narjis Alhusseini",
+        photo: "",
+        position: "Co-Director",
+        major: "Microbiology and Immunology",
+      },
+    ],
+  },
+  {
+    header: "Leads",
+    people: [
+      {
+        name: "Stella Kauryzhka",
+        photo: "",
+        position: "Wet Lab",
+        major: "Biomedical Engineering",
+      },
+      {
+        name: "Tina Wang",
+        photo: "",
+        position: "Wet Lab",
+        major: "Microbiology and Immunology",
+      },
+      {
+        name: "Lucy Hao",
+        photo: "",
+        position: "Dry Lab and Wiki",
+        major: "Computer Science and Chemistry",
+      },
+      {
+        name: "Piyush Awasthi",
+        photo: "",
+        position: "Dry Lab",
+        major: "Biomedical Engineering",
+      },
+      {
+        name: "Harrison Kim",
+        photo: "",
+        position: "Human Practices",
+        major: "Biomedical Engineering",
+      },
+      {
+        name: "Yejin Lhee",
+        photo: "",
+        position: "Design and External",
+        major: "Biochemistry",
+      },
+      {
+        name: "Achint Lail",
+        photo: "",
+        position: "Finance",
+        major: "Honours in Chemistry and Biochemistry",
+      },
+    ],
+  },
+  {
+    header: "Wet Lab",
+    people: [
+      {
+        name: "Achint Lail",
+        photo: "",
+        major: "Honours in Chemistry and Biochemistry",
+      },
+      {
+        name: "Ada Jiang",
+        photo: "",
+        major: "Microbiology and Immunology",
+      },
+      {
+        name: "Burak Ozkan",
+        photo: "",
+        major: "Microbiology and Immunology",
+      },
+      {
+        name: "Chloe King",
+        photo: "",
+        major: "Applied Biology",
+      },
+      {
+        name: "Daniel Hinatsu",
+        photo: "",
+        major: "Biomedical Engineering",
+      },
+      {
+        name: "Diego Perez Hidalgo",
+        photo: "",
+        major: "Honours Biochemistry",
+      },
+      {
+        name: "Pattarin Blanchard",
+        photo: "",
+        major: "Chemical and Biological Engineering",
+      },
+      {
+        name: "Qinru Kong",
+        photo: "",
+        major: "Microbiology and Immunology",
+      },
+      {
+        name: "Ran Tao",
+        photo: "",
+        major: "Microbiology and Immunology",
+      },
+    ],
+  },
+  {
+    header: "Dry Lab",
+    people: [
+      {
+        name: "Matthias Wong",
+        photo: "",
+        major: "Biomedical Engineering",
+      },
+      {
+        name: "Riya Alluri",
+        photo: "",
+        major: "Biology and Computer Science",
+      },
+      {
+        name: "Samuel Salitra",
+        photo: "",
+        major: "Microbiology and Immunology and Computer Science",
+      },
+      {
+        name: "Sebastian Hyland",
+        photo: "",
+        major: "Applied Science",
+      },
+    ],
+  },
+  {
+    header: "Human Practices",
+    people: [
+      {
+        name: "Charlotte Lee",
+        photo: "",
+        major: "Microbiology and Immunology",
+      },
+      {
+        name: "Claire Pinckney",
+        photo: "",
+        major: "Science",
+      },
+      {
+        name: "Jessica Xin",
+        photo: "",
+        major: "Biomedical Engineering",
+      },
+      {
+        name: "Yejin Lhee",
+        photo: "",
+        major: "Biochemistry",
+      },
+    ],
+  },
+  {
+    header: "Wiki",
+    people: [
+      {
+        name: "Deigo Perez Hidalgo",
+        photo: "",
+        major: "Honours Biochemistry",
+      },
+    ],
+  },
+  {
+    header: "Design",
+    people: [
+      {
+        name: "Aoniya Colynn",
+        photo: "",
+        major: "Science",
+      },
+      {
+        name: "Karen Lin",
+        photo: "",
+        major: "Visual Arts",
+      },
+    ],
+  },
+  {
+    header: "Advisors",
+    people: [
+      {
+        name: "Laura Gonzalez Campos",
+        photo: "",
+        major: "Graduate Advisor",
+      },
+      {
+        name: "Rodrigo Vallejos",
+        photo: "",
+        major: "Graduate Advisor",
+      },
+      {
+        name: "Anjali Parthasarathy",
+        photo: "",
+        major: "Undergraduate Advisor",
+      },
+      {
+        name: "Brenda Ma",
+        photo: "",
+        major: "Undergraduate Advisor",
+      },
+      {
+        name: "Edward Li",
+        photo: "",
+        major: "Undergraduate Advisor",
+      },
+      {
+        name: "Parneet Sekhon",
+        photo: "",
+        major: "Undergraduate Advisor",
+      },
+      {
+        name: "Umar Ali",
+        photo: "",
+        major: "Undergraduate Advisor",
+      },
+    ],
+  },
+  {
+    header: "Principal Investigator",
+    people: [
+      {
+        name: "Dr. Steven Hallam",
+        photo: "",
+        major: "Faculty Advisor",
+      },
+      {
+        name: "Dr. David Oliver",
+        photo: "",
+        major: "Faculty Advisor",
+      },
+    ],
+  },
+];
--- src/routes/team.jsx
+++ src/routes/team.jsx
@@ -1,21 +1,115 @@
+import styled from "styled-components";
 import Footer from "../components/footer";
 import Header from "../components/header";
+import { Members, Padding } from "../constants";
+import TeamBackground from "../images/team/team_background.png";
+import Button from "../components/button";
+
+const Background = styled.div`
+  background-image: url(${TeamBackground});
+  background-repeat: no-repeat;
+  min-height: fit-content;
+  background-size: cover;
+  background-position-y: -7%;
+`;
+
+const Col = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 12vw;
+  height: 12vw;
+  margin: 1vw;
+  padding: 1vw;
+  border-radius: 60px;
+  background-color: #b1cbc0;
+  justify-content: center;
+`;
+
+const FlexRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  /* justify-content: space-around; */
+`;
+
+const Text = styled.p`
+  margin: 0;
+  padding: 0px;
+  font-size: 20px;
+  text-align: center;
+`;
 
 export default function Team() {
   return (
-    <div>
+    <Background>
       <Header />
-      <div>
-        <h1>Meet our Team</h1>
-        <p>
-          UBC iGEM is made up of passionate undergraduate scientists from
-          different departments and faculties
-        </p>
-      </div>
-      <div>
-        <p>TODO</p>
-      </div>
-      <Footer/>
-    </div>
+      <Padding>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "col",
+            justifyContent: "right",
+            paddingTop: "12vw",
+          }}
+        >
+          <div style={{ width: "45vw", paddingBottom: "10vw" }}>
+            <h1>Meet our Team</h1>
+            <p>
+              UBC iGEM is made up of passionate undergraduate scientists from
+              different departments and faculties
+            </p>
+          </div>
+        </div>
+
+        <div style={{ paddingBottom: "10vw" }}>
+          {Members.map((members) => (
+            <>
+              <h2>{members.header}</h2>
+              <FlexRow style={{ paddingBottom: "5vw" }}>
+                {members.people.map(({ name, position, major }) => (
+                  <Col>
+                    <Text>{name}</Text>
+                    <div style={{ paddingTop: "15px" }}>
+                      <Text>{position}</Text>
+                      <Text>
+                        <i>{major}</i>
+                      </Text>
+                    </div>
+                  </Col>
+                ))}
+              </FlexRow>
+            </>
+          ))}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            textAlign: "center",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              paddingTop: "10vw",
+              paddingBottom: "10vw",
+            }}
+          >
+            <h2>Want to join us?</h2>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                textAlign: "center",
+              }}
+            >
+              <Button text="Learn More" link="/join" />
+            </div>
+          </div>
+        </div>
+      </Padding>
+      <Footer />
+    </Background>
   );
 }
