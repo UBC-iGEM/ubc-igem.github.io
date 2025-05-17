769c8673c8d569e0d2cc4218438008d616a2d3eb
Gallections <Gallections@gmail.com>
2025-05-17 13:55:31 -0700

    feat: adding styles to past-projects-page

---
+++ src/data/pastProjects.ts
@@ -0,0 +1,182 @@
+export const pastProjects = [
+    {
+        title: "nuCloud",
+        description: "The 2024 team developed a terminal deoxynucleotidyl transferase (TdT)-based solid phase DNA synthesis platform that allows users to synthesize data-encoding DNA.",
+        icon: "../../../static/past-project-icons/2024_nuCloud.gif",
+        awards: [
+          { icon: "../../../static/past-project-icons/goldMedal.png", description: "GOLD MEDAL" },
+          { icon: "../../../static/past-project-icons/bestHardware.png", description: "BEST HARDWARE" },
+          { icon: "../../../static/past-project-icons/bestSusDev.png", description: "BEST SUSTAINABLE DEVELOPEMENT IMPACT" },
+          { icon: "../../../static/past-project-icons/nominee_bestBioManufacture.svg", description: "BEST BIOMANUFACTURING PROJECT NOMINEE" }
+        ],
+        link: "https://2024.igem.wiki/ubc-vancouver/",
+        year: "2024"
+    },
+    {
+        title: "PILOT",
+        description: "The 2023 team developed a modular intein-based cell-free protein synthesis (CFPS) platform that uses a strain of Vibrio natriegens to improve protein production/purification efficiency and energy usage.",
+        icon: "../../../static/past-project-icons/2023_PILOT.png",
+        awards: [
+          { icon: "../../../static/past-project-icons/silverMedal.svg", description: "SILVER MEDAL" },
+          ],
+        link: "https://2023.igem.wiki/ubc-vancouver/",
+        year: "BES"
+    },
+    {
+        title: "Synaestivum",
+        description: "The 2022 team engineered a strain of heat-resistant wheat to tackle global increases in temperature. A proof of concept protoplast system demonstrated the successful integration of our genes of interest.",
+        icon: "../../../static/past-project-icons/2022_Synaestivum.png",
+        awards: [
+          { icon: "../../../static/past-project-icons/goldMedal.png", description: "GOLD MEDAL" },
+          ],
+        link: "https://2022.igem.wiki/ubc-vancouver/",
+        year: "2022"
+    },
+    {
+        title: "DetecTME",
+        description: "The 2021 team engineered a non-pathogenic, tumour-colonizing Salmonella strain to report anti-tumour immune activity within the tumour microenviornment. The expression of a reporter molecule would be recovered from the patient’s urine.",
+        icon: "../../../static/past-project-icons/2021_detecTME.png",
+        awards: [
+            { icon: "../../../static/past-project-icons/nominee_bestDignostic.svg", description: "BEST DIAGNOSTICS PROJECT NOMINEE" },
+          { icon: "../../../static/past-project-icons/goldMedal.png", description: "GOLD MEDAL" },
+          ],
+        link: "https://2021.igem.org/Team:British_Columbia",
+        year: "2021"
+    }, 
+    {
+        title: "VPRE",
+        description: "The 2020 team built a machine learning pipeline to predict the evolution of SARS-CoV-2 to inform vaccine target selection for COVID-19.",
+        icon: "../../../static/past-project-icons/2020_VPRE.svg",
+        awards: [
+            { icon: "../../../static/past-project-icons/planarySpeechHarvard.svg", description: "Plenary speech at Harvard’s National Collegiate Research Conference and Stanford Research" },
+          ],
+        link: "https://2020.igem.org/Team:British_Columbia", // There should be no link here?
+        year: "2020"
+
+    },
+    {
+        title: "Paralyte-STX",
+        description: "The 2019 team built a transcription-based biosensor to detect levels of saxitoxin, a toxin responsible for paralytic shellfish poisoning in humans. The resulting high-throughput screening would be used as an on-site device for detection of the toxin in bodies of water.",
+        icon: "../../../static/past-project-icons/2019_Paralyte-STX.png",
+        awards: [
+          ],
+        link: "https://2019.igem.org/Team:British_Columbia",
+        year: "2019"
+    }, 
+    {
+        title: "COOptimize",
+        description: "The 2018 team used bacterial co-cultures to produce Naringenin and Kaempferol, biochemicals with anti-cancer properties. Distributing biosynthesis between two strains would both lighten the metabolic load and maximize energetic efficiency of the cell.",
+        icon: "../../../static/past-project-icons/2018_COOptimize.svg",
+        awards: [
+          { icon: "../../../static/past-project-icons/bronzeMedal.svg", description: "BRONZE MEDAL" },
+          ],
+        link: "https://2018.igem.org/Team:British_Columbia",
+        year: "2018"
+    }, 
+
+    {
+        title: "aGROW",
+        description: "The 2017 team used CRISPR/Cas9 to remove the gene responsible for plant death and agricultural loss in Agrobacteria tumefaciens. Their transgenic bacteria would conjugate with A. tumefaciens, rendering it harmless and either preventing or correcting damage caused by it.",
+        icon: "../../../static/past-project-icons/2017_aGROW.png",
+        awards: [
+            { icon: "../../../static/past-project-icons/silverMedal.svg", description: "SILVER MEDAL" },
+            { icon: "../../../static/past-project-icons/nominee_bestEnviro.svg", description: "BEST ENVIRONMENT PROJECT NOMINEE" },
+          { icon: "../../../static/past-project-icons/nominee_bestModel.svg", description: "BEST MODEL NOMINEE" },
+          ],
+        link: "https://2017.igem.org/Team:British_Columbia",
+        year: "2017"
+    },
+    
+    {
+        title: "Crescentium",
+        description: "Plant biomass has potential as a renewable energy source, but the metabolic strain of its degradation by a single strain is too large to accomplish this. The 2016 team developed a microbial community that would both degrade and valorize the biomass.",
+        icon: "../../../static/past-project-icons/2016_Crescentium.png",
+        awards: [
+            { icon: "../../../static/past-project-icons/nominee_bestManufacture.svg", description: "BEST MANUFACTURING PROJECT NOMINEE" },
+            { icon: "../../../static/past-project-icons/goldMedal.png", description: "GOLD MEDAL" },
+          ],
+        link: "https://2017.igem.org/Team:British_Columbia",
+        year: "2017"
+    }, 
+
+    {
+        title: "Probeeotics",
+        description: "Honeybee Colony Collapse Disorder destroys whole bee colonies, where neonicotinoid pesticides poison plat-feeders like bees. The 2015 team engineered honeybee intestinal bacteria to degrade imidacloprid, a widely-used neonicotinoid.",
+        icon: "../../../static/past-project-icons/2015_Probeeotics.png",
+        awards: [
+            { icon: "../../../static/past-project-icons/nominee_bestEnviro.svg", description: "BEST ENVIRONMENTAL PROJECT NOMINEE" },
+            { icon: "../../../static/past-project-icons/goldMedal.png", description: "GOLD MEDAL" },
+          ],
+        link: "https://2015.igem.org/Team:British_Columbia",
+        year: "2015"
+    }, 
+
+    {
+        title: "Darwin’s Metals",
+        description: "Mineral separation in copper mining is in increasing demand but uses chemically taxing measures. The 2014 iGEM team used octapeptides in bacteria to selectively bind chalocopyrite, a copper mineral that separates it from the rest of the minerals.",
+        icon: "../../../static/past-project-icons/2014_DarwinMetals.png",
+        awards: [
+            { icon: "../../../static/past-project-icons/goldMedal.png", description: "GOLD MEDAL" },
+          ],
+        link: "https://2014.igem.org/Team:British_Columbia",
+        year: "2014"
+    }, 
+
+    {
+        title: "CRISPR MADE BY U",
+        description: "Engineered a minimal CRISPR system in E. coli with broad-spectrum phage resistance by analyzing viral sequences, demonstrating a proof-of-concept for customizable bioreactor immunity and providing modular tools for future bacterial CRISPR applications.",
+        icon: "../../../static/past-project-icons/2013_CRISPRMADEBYU.png",
+        awards: [
+            { icon: "../../../static/past-project-icons/goldMedal.png", description: "GOLD MEDAL" },
+            { icon: "../../../static/past-project-icons/bestModel.png", description: "BEST MODEL" }
+          ],
+        link: "https://2013.igem.org/Team:British_Columbia",
+        year: "2013"
+    }, 
+
+    {
+        title: "Synthetic Syntrophy",
+        description: "Engineered a tunable consortium with a distributed 4S desulfurization pathway for increased efficiency in the removal of organosulfurs in heavy oils and bitumen resources.",
+        icon: "../../../static/past-project-icons/2012_SyntheticSyntrophy.jpg",
+        awards: [
+            { icon: "../../../static/past-project-icons/goldMedal.png", description: "GOLD MEDAL" }
+          ],
+        link: "https://2012.igem.org/Team:British_Columbia",
+        year: "2012"
+    }, 
+
+    {
+        title: "iSynthase",
+        description: "Optimized the production of terpenes in Saccharomyces cerevisiae yeast by constructing the biosynthetic pathways necessary to synthesize these compounds. Developed models of (i) monoterpene synthase structure, (ii) monoterpene production in yeast and (iii) the dynamics of the mountain pine beetle populations in British Columbia under the influence of our synthetic yeast..",
+        icon: "../../../static/past-project-icons/2011_iSynthase.jpg",
+        awards: [
+            { icon: "../../../static/past-project-icons/goldMedal.png", description: "GOLD MEDAL" }
+          ],
+        link: "https://2011.igem.org/Team:British_Columbia",
+        year: "2011"
+    }, 
+
+    {
+        title: "Blasting Away Biofilms",
+        description: "Expressed an endogenous bacteriophage and biofilm matrix-degrading enzyme DspB under the control of the agr quorum-sensing system to disperse Staphylococcus aureus biofilms.",
+        icon: "../../../static/past-project-icons/2010_BlastingAwayBiofilms.png",
+        awards: [
+            { icon: "../../../static/past-project-icons/goldMedal.png", description: "GOLD MEDAL" }
+          ],
+        link: "https://2010.igem.org/Team:British_Columbia",
+        year: "2010"
+    }, 
+
+    {
+        title: "E. coli Traffic Light",
+        description: "Created a biosensor that recognizes a specific target and alters its output fluorescence from green, to yellow, to red as a function of concentration up to critical levels (hence, a biological \"traffic light\").",
+        icon: "../../../static/past-project-icons/2009_EcoliTrafficLight.png",
+        awards: [
+          ],
+        link: "https://2009.igem.org/Team:British_Columbia",
+        year: "2009"
+    }, 
+
+
+
+]
