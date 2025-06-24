9315aab059fd528f16e63e74e585936374fafbb8
Gallections <Gallections@gmail.com>
2025-06-23 20:26:15 -0700

    feat: add the rest of the current sponsors logo

--- src/data/currentSponsors.ts
+++ src/data/currentSponsors.ts
@@ -5,5 +5,21 @@ export const currentSponsors = [
             "SnapGene_Logo.svg",
             "Geneious_Logo.svg"
         ]
+    }, 
+    {
+        tier: "SILVER",
+        icons: [
+            "UBC_Science.svg",
+            "UBC_Department_Microbiology_Immunology.svg"
+        ]
+    },
+    {
+        tier: "BRONZE",
+        icons: [
+            "UBC_SUS.svg",
+            "UBC_Applied_Science.png",
+            "UBC_Life_Sciences.svg",
+            "UBC_Zoology.svg"
+        ]
     }
 ]
