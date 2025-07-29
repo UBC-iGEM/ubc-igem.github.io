37c22295fba6cf4185bfba91e544501cfc7a3566
ThatNerdSquared <72814106+ThatNerdSquared@users.noreply.github.com>
2025-07-29 00:37:37 -0700

    chore: add eslint and prettier support

---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+import js from "@eslint/js"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+import { defineConfig } from "eslint/config"
+import eslintPluginAstro from "eslint-plugin-astro"
+
+// can't use @eslint/css with the astro eslint plugin - see this gh issue for
+// details: https://github.com/ota-meshi/eslint-plugin-astro/issues/485
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginAstro.configs["jsx-a11y-strict"],
+  {
+    rules: {
+      "default-case-last": "error",
+      eqeqeq: "error",
+      "no-await-in-loop": "warn",
+      "no-duplicate-imports": "error",
+      "no-implicit-coercion": "error",
+      "no-label-var": "error",
+      "no-self-compare": "error",
+      "no-template-curly-in-string": "error",
+      "no-unmodified-loop-condition": "error",
+      "no-unreachable-loop": "error",
+      "no-useless-assignment": "error",
+      "prefer-promise-reject-errors": "error",
+      yoda: "warn",
+    },
+  },
+])
